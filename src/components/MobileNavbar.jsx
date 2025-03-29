fe74cc96fc1876ac3c72fdd25cd240288fdd4672
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-03-29 15:42:11 +0530

    added hamburger working feature

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -67,6 +67,7 @@ const MobileNavbar = () => {
                 </Link>
                 <button
                     type="button"
+                    onClick={() => window.open("https://drive.google.com/file/d/1E3rTfbKlV-pV-OatSrRZOOaa9Hxq94Uc/view?usp=sharing", "_blank")}
                     className="block sm:hidden text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Download CV</button>
             </div>
         </div>
