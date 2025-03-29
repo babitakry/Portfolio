8a2c8508b24f6cbd8f74bcdb9ad3ea44e0e07f62
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-03-29 15:38:55 +0530

    added hire me

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -33,7 +33,7 @@ function Introduction() {
                 Here you'll learn about my journery as web developer and a Software Developer.
                 </p>
                 <div>
-                    <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 ">Hire Me</button>
+                    <a href='mailto:babitakumari.ky3007@gmail.com' className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 ">Hire Me</a>
                 </div>
             </div>
             <div className='sm:w-[48%] sm:h-full flex flex-col'>
